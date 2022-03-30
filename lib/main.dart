d3cd1b81dd3463a2cd6cf71c8408648edb0cc894
Jeff Avery <jeffery.avery@uwaterloo.ca>
2022-03-30 14:21:43 -0400

    Minor tweaks to UI and class names

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WellnessApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class WellnessApp extends StatelessWidget {
+  const WellnessApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
