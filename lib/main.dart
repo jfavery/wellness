d0b7ab4572966c4621fe547ccf58eb5454d011d7
Jeff Avery <jeffery.avery@uwaterloo.ca>
2022-04-14 12:03:02 -0400

    Fixing height, font issues.

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class WellnessApp extends StatelessWidget {
       title: 'Wellness',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.yellow,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
@@ -32,7 +32,25 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Wellness Companion'),
+        toolbarHeight: 100,
+        title: Column(
+            children: <Widget>[
+              SizedBox(height: 10),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Text('The Wellness Circle', style: TextStyle(fontSize: 20))
+              ),
+              SizedBox(height: 10),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                    'wisdom◦love◦respect◦bravery◦honesty◦humility◦truth',
+                    style: TextStyle(fontSize: 13)
+                )
+              ),
+              SizedBox(height: 10),
+            ]),
+        foregroundColor: Colors.black,
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -41,23 +59,25 @@ class _MyHomePageState extends State<MyHomePage> {
           // const Text('Instructions can go here',
           //     textAlign: TextAlign.center, style: TextStyle(fontSize: 22)),
           DefaultTabController(
-            length: 4, // length of tabs
+            length: 5, // length of tabs
             initialIndex: 0,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 const TabBar(
                   labelColor: Colors.green,
+                  labelStyle: TextStyle(fontSize: 10.0,fontFamily: 'Arial'),
                   unselectedLabelColor: Colors.black,
                   tabs: [
-                    Tab(text: 'Tab 1'),
-                    Tab(text: 'Tab 2'),
-                    Tab(text: 'Tab 3'),
-                    Tab(text: 'Tab 4'),
+                    Tab(text: 'Mental\nHealth'),
+                    Tab(text: 'Physical\nHealth'),
+                    Tab(text: 'Spiritual\nHealth'),
+                    Tab(text: 'Emotional\nHealth'),
+                    Tab(text: 'Calendar')
                   ],
                 ),
                 Container(
-                  height: 700, //height of TabBarView
+                  height: 650, //height of TabBarView
                   decoration: const BoxDecoration(
                       border: Border(
                           top: BorderSide(color: Colors.grey, width: 0.5))),
@@ -65,7 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     children: <Widget>[
                       Align (
                         alignment: Alignment.topLeft,
-                        child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+                        child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacommodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa qui officia deserunt mollit anim id est laborum.',
                           style: TextStyle(
                                 fontSize: 21, fontWeight: FontWeight.normal
                             ),
@@ -93,6 +113,14 @@ class _MyHomePageState extends State<MyHomePage> {
                             style: TextStyle(
                                 fontSize: 21, fontWeight: FontWeight.normal
                             ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text('At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
+                            style: TextStyle(
+                                fontSize: 21, fontWeight: FontWeight.normal
+                            )
                         ),
                       ),
                     ],
